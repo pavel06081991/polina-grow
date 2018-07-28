d965832a13baa2a0e5c08cde0310127870105b53
Palina Kartsel <Palina_Kartsel@epam.com>
2018-07-28 11:36:08 +0300

    wrapped app in redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './redicers/reducer'
+
+const store = createStore(reducer);
 
 const Index = () => {
-  return <div>Hello, guys!</div>;
+  return (
+    <Provider store={}>
+      <div>Hello, guys!</div>
+    </Provider>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
